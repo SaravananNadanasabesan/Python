898c4f9a0af87d85c00bb93e3f42166d5781c281
SaravananNadanasabesan <saravanasabesan@gmail.com>
2025-06-12 10:43:59 -0400

    count practice

---
+++ count.py
@@ -0,0 +1,10 @@
+# Print values 0 to 7.
+for count in range(8):
+    print(count)
+# Print values 2 to 7.
+for count in range(2, 8):
+    print(count)
+# Print 2, 4, 6.
+for count in range(2, 8,
+2):
+    print(count)
